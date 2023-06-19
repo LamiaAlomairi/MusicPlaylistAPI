8f887993ecce3983d75dfda07c53d04bc88c41cd
unknown <lama5alomairi@gmail.com>
2023-06-19 10:56:14 +0400

    Create All Packages(Controllers, Services, Repositories, Models, ResponseObject, RequestObject)

---
+++ src/main/java/com/MusicPlaylistAPI/MusicPlaylistAPI/MusicPlaylistApiApplication.java
@@ -0,0 +1,13 @@
+package com.MusicPlaylistAPI.MusicPlaylistAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlaylistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlaylistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MusicPlaylistAPI/MusicPlaylistAPI/MusicPlaylistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MusicPlaylistAPI.MusicPlaylistAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlaylistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
