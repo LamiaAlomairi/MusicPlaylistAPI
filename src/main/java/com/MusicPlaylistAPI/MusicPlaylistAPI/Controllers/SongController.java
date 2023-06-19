1ea39d2cf697253f363b3ff8d7f0df69ec07e11d
unknown <lama5alomairi@gmail.com>
2023-06-19 12:34:54 +0400

    Create SongController and addSong method

---
+++ src/main/java/com/MusicPlaylistAPI/MusicPlaylistAPI/Controllers/SongController.java
@@ -0,0 +1,22 @@
+package com.MusicPlaylistAPI.MusicPlaylistAPI.Controllers;
+
+import com.MusicPlaylistAPI.MusicPlaylistAPI.RequestObject.SongRequest;
+import com.MusicPlaylistAPI.MusicPlaylistAPI.Services.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/playlists/{id}/songs")
+public class SongController {
+    @Autowired
+    SongService songService;
+
+    /*******  Song Addition  ******/
+    @PostMapping
+    public void addSong(@RequestBody SongRequest songRequest) {
+        songService.addSong(songRequest);
+    }
+}
